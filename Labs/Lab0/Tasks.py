6c6e361bc16b60af1fdea4ae5c12a894fbb7ecd1
alexmccorkle <114952775+alexmccorkle@users.noreply.github.com>
2024-09-02 09:24:25 +0200

    Lab0

---
+++ Labs/Lab0/Tasks.py
@@ -0,0 +1,48 @@
+
+
+# Task 1
+print ("Hello World")
+
+
+# Task 2
+# Addition
+def addition(a, b):
+    return a + b
+
+# Subtraction
+def subtract(a, b):
+    return a - b
+
+# Multiplication
+def multi(a, b):
+    return a * b
+
+# Division
+def divide(a, b):
+    return a / b
+
+print (addition(1, 2))
+print (subtract(5, 3))
+print (multi(2, 3))
+print (divide(10, 2))
+
+
+# Task 3
+name = "Alex"
+age = 28
+
+print ("Me llamo " + name + " y yo soy " + str(age) + " anos")
+
+# Task 4
+for i in range(1, 11):
+    print (i) 
+
+# Task 5
+def isGreaterThan5(a):
+    if a > 5: 
+        return True
+    return False
+
+print (isGreaterThan5(6))
+print (isGreaterThan5(3))
+
